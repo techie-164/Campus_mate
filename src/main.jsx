22bbae9e93d197ce461b07f7c210a90c7f61ea8a
Prasad Patil <prasadspatil1234@gmail.com>
2026-06-01 11:41:44 +0530

    Routing added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { RouterProvider, Routes, createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, createRoutesFromElements} from "react-router-dom";
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
 import App from './App.jsx'
+import './index.css'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/home" element={<App />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
